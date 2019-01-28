90efb75bda9213634642531f3f3bc720ccd2bfa7
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2019-01-28 14:22:24 +0100

    Add files via upload

---
+++ rasberrypiPlantautomation.py
@@ -0,0 +1,23 @@
+import datetime
+import time
+
+
+def __init__(self, waterc, lightc, pump):
+    self.watercycle = waterc
+    self.pumpcycle = pump
+    self.lightcycle = lightc
+
+
+#water cycle
+while True:
+     datetime.date(now) = True
+
+
+
+#pump time (seconds) relative to pump intensity
+    pump = t_end = time.time() + 5
+        while True: 
+            time.time() < t_end:
+        dt = datetime.timedelta(365)
+    
+
