a1abd1f632124792ec51f4636126214d53170889
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2019-04-01 22:43:36 +0200

    Update rasberrypiPlantautomation.py

--- rasberrypiPlantautomation.py
+++ rasberrypiPlantautomation.py
@@ -1,49 +1,15 @@
 import datetime
-from apscheduler import BlockingScheduler
-import GPIO as io
-import RPi.GPIO as GPIO
-import signal
-import subprocess
-import os
-from time import sleep
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
-
-rec_proc = None
-try:
-    while True:
-        if GPIO.input(4):
-            GPIO.output(25, 1)
-            if rec_proc is None:
-                rec_proc = subprocess.Popen("/script/start.sh",
-                           shell=True, preexec_fn=os.setsid)
-        else:
-            GPIO.output(25, 0)
-            if rec_proc is not None:
-                os.killpg(rec_proc.pid, signal.SIGTERM)
-                rec_proc = None
-        sleep(0.2)
-
-finally:
-    GPIO.cleanup()
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+import Rpi.GPIO as io
+import threading
 
 
-time.sleep(.1)
+def __init__(self, watercycle, lightcycle, pumpcycle):
+    self.watercycle = watercycle
+    self.pumpcycle = pumpcycle
+    self.lightcycle = lightcycle
 
-class(components):
-    def __init__(self, watercycle, lightcycle, pump, io, apscheduler, datetime):
-        self.watercycle = watercycle
-        self.pump = pump
-        self.lightcycle = lightcycle
-        self.io = io
-        assert isinstance(apscheduler, BlockingScheduler)
-        self.apscheduler = apscheduler
-        self.z = datetime
-
-
-pump_start = io
 
 # water cycle
 while True:
@@ -52,16 +18,21 @@ while True:
 
     # pump time (seconds) Adjust according to pump intensity and diameter of tubing
     if True:
-        pump.pump = time.time() + 5
+        pump.pumpcycle = time.time() + 5
     then
 
-    1 < time.clock()
 
-    pump_Start
+    pumpcycle_start()
+    
+    if False:
+    
+    then
+    
+    pumpcycle.stop()
+    
 else:
         pump_Stop
         dt = datetime.timedelta(365)
-
 
         # light cycle (12 hour schedule) using apscheduler let's specific sections of the code run at set time intervals, useful for different cycles
 
@@ -94,4 +65,4 @@ else:
             io.output(5, 0)
             time.sleep(0, 30)
             io.output(5, 1)
-time.sleep(0, 30)
+            time.sleep(0, 30)
